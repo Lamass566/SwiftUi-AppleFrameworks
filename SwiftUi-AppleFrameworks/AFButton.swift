6e9528dd05a458abcbb834395d389559a417e8f1
Misha <vsisoev@ukr.net>
2024-05-11 17:07:38 +0300

    created a detail view

---
+++ SwiftUi-AppleFrameworks/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  SwiftUi-AppleFrameworks
+//
+//  Created by My Mac on 11.05.2024.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
---
+++ SwiftUi-AppleFrameworks/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  SwiftUi-AppleFrameworks
+//
+//  Created by My Mac on 11.05.2024.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
