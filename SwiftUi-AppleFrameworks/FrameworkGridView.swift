98be3cb04951027528bb5620a93dea44f4bce1f2
Misha <vsisoev@ukr.net>
2024-05-11 15:48:08 +0300

    Initial Commit

---
+++ SwiftUi-AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(MockData.frameworks){ framework in
+                        FrameworkTitleView(framework: framework)
+                            .padding(.top, 20)
+                    }
+                    
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+            
+        }
+    }
+}
+
+#Preview {  
+    FrameworkGridView()
+}
+
+struct FrameworkTitleView: View{
+    let framework: Framework
+    
+    var body: some View{
+        VStack{
+            Image(framework.imageName)
+                    .resizable()
+                    .frame(width: 90, height: 90)
+            Text(framework.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .scaledToFit()
+                    .minimumScaleFactor(0.6)
+            }
+    }
+    
+}
---
+++ SwiftUi-AppleFrameworks/SwiftUi_AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_AppleFrameworksApp.swift
+//  SwiftUi-AppleFrameworks
+//
+//  Created by My Mac on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
