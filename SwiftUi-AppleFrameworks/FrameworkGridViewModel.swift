1188cdc84be1570d60269c438509da00b8a9504c
Misha <vsisoev@ukr.net>
2024-05-12 12:05:30 +0300

    added safariView and model for gridView

---
+++ SwiftUi-AppleFrameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkGridViewModel.swift
+//  SwiftUi-AppleFrameworks
+//
+//  Created by My Mac on 11.05.2024.
+//
+
+import Foundation
---
+++ SwiftUi-AppleFrameworks/SafariView.swift
@@ -0,0 +1,8 @@
+//
+//  SafariView.swift
+//  SwiftUi-AppleFrameworks
+//
+//  Created by My Mac on 12.05.2024.
+//
+
+import Foundation
